dfa41c782b559a9ecede7d5eb12a508b8c06e2b5
Sam Germain <sam.germain@usask.ca>
2021-06-06 02:22:38 -0600

    removed util folder

--- src/util/max_width.js
+++
@@ -1,8 +0,0 @@
-//$( document ).ready( function(){
-//	setMaxWidth();
-
-//	function setMaxWidth() {
-//		$( "#everything" ).css( "maxWidth", ( $( window ).width() * 0.5 | 0 ) + "px" );
-//	}
-
-//});
